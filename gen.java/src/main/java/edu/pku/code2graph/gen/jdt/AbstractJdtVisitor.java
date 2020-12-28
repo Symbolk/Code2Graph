76c46816645cf10191e912ae4d53427bb7a40722
Symbolk <symbolk@163.com>
2020-12-28 23:27:07 +0800

    feat: add edge type for method and corresponding cache data structure.

--- gen.java/src/main/java/edu/pku/code2graph/gen/jdt/AbstractJdtVisitor.java
+++ gen.java/src/main/java/edu/pku/code2graph/gen/jdt/AbstractJdtVisitor.java
@@ -1,12 +1,15 @@
 package edu.pku.code2graph.gen.jdt;
 
-import edu.pku.code2graph.model.Edge;
-import edu.pku.code2graph.model.Node;
-import edu.pku.code2graph.model.Type;
+import edu.pku.code2graph.gen.jdt.model.EdgeType;
+import edu.pku.code2graph.model.*;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.jgrapht.Graph;
+import org.jgrapht.alg.util.Pair;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static edu.pku.code2graph.model.TypeSet.type;
 
@@ -14,8 +17,13 @@ public abstract class AbstractJdtVisitor extends ASTVisitor {
   // final constructed graph instance
   protected Graph<Node, Edge> graph = initGraph();
 
-  // index nodes by qualified name Trie to speed up matching
-  // or simply use hash?
+  // TODO index nodes by qualified name as Trie to speed up matching
+  // TODO include external type declaration or not?
+  // intermediate cache to build nodes and edges
+  // basic assumption: qualified name is unique in one project
+  // A binding represents a named entity in the Java language
+  protected Map<String, ElementNode> elementPool = new HashMap<>();
+  protected Map<OperationNode, Pair<EdgeType, String>> operationPool = new HashMap<>();
 
   public AbstractJdtVisitor() {
     super(true);
--- gen.java/src/main/java/edu/pku/code2graph/gen/jdt/model/EdgeType.java
+++ gen.java/src/main/java/edu/pku/code2graph/gen/jdt/model/EdgeType.java
@@ -5,10 +5,14 @@ import edu.pku.code2graph.model.Type;
 import static edu.pku.code2graph.model.TypeSet.type;
 
 public class EdgeType {
+    // method declaration
+    public static final Type METHOD_PARAMETER = type("METHOD_PARAMETER");
+    public static final Type METHOD_RETURN = type("METHOD_RETURN");
+    public static final Type METHOD_EXCEPTION = type("METHOD_EXCEPTION");
+
+    // method invocation
     public static final Type METHOD_CALLER = type("METHOD_CALLER");
     public static final Type METHOD_CALLEE = type("METHOD_CALLEE");
     public static final Type METHOD_ARGUMENT = type("METHOD_ARGUMENT");
-
-    public static final Type METHOD_RETURN = type("METHOD_RETURN");
 
 }
