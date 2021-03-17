00b6bd27238abc00f17c4c68f12ddd52ac6dd991
Symbolk <symbolk@163.com>
2021-03-17 17:28:30 +0800

    kotlin: add modules for kotlin parsing.

---
+++ gen.kotlin/src/main/kotlin/KtGenerator.kt
@@ -0,0 +1,21 @@
+package kotlinx.ast.example
+
+import kotlinx.ast.common.AstSource
+import kotlinx.ast.common.ast.Ast
+import kotlinx.ast.common.print
+import kotlinx.ast.grammar.kotlin.common.summary
+import kotlinx.ast.grammar.kotlin.target.antlr.kotlin.KotlinGrammarAntlrKotlinParser
+
+fun main() {
+    val source = AstSource.File(
+            "gen.kotlin/src/main/resources/Example.kt.txt"
+    )
+    val kotlinFile = KotlinGrammarAntlrKotlinParser.parseKotlinFile(source)
+    kotlinFile.summary(attachRawAst = false)
+            .onSuccess { astList ->
+                astList.forEach(Ast::print)
+            }.onFailure { errors ->
+                errors.forEach(::println)
+            }
+}
+
