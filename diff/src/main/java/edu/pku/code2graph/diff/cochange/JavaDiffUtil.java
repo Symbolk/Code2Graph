814477fa1c06418008ca093ff81d8daae100a425
Symbolk <symbolk@163.com>
2021-04-23 12:12:57 +0800

    fix: remove buggy Gumtree $ for inner types.

--- diff/src/main/java/edu/pku/code2graph/diff/cochange/JavaDiffUtil.java
+++ diff/src/main/java/edu/pku/code2graph/diff/cochange/JavaDiffUtil.java
@@ -6,6 +6,7 @@ import edu.pku.code2graph.diff.model.DiffFile;
 import gumtree.spoon.AstComparator;
 import gumtree.spoon.diff.Diff;
 import gumtree.spoon.diff.operations.Operation;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Triple;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtTypeMember;
@@ -64,12 +65,14 @@ public class JavaDiffUtil {
     CtElement element = operation.getSrcNode();
     if (element instanceof CtTypeImpl) {
       return Triple.of(
-          getChangeType(operation.getAction()), ((CtTypeImpl) element).getQualifiedName(), "");
+          getChangeType(operation.getAction()),
+          convertTypeName(((CtTypeImpl) element).getQualifiedName()),
+          "");
     } else if (element instanceof CtTypeMember) {
       CtTypeMember member = (CtTypeMember) element;
       return Triple.of(
           getChangeType(operation.getAction()),
-          member.getDeclaringType().getQualifiedName(),
+          convertTypeName(member.getDeclaringType().getQualifiedName()),
           member.getSimpleName());
     } else {
       // find parent member and type
@@ -84,12 +87,23 @@ public class JavaDiffUtil {
       }
 
       String parentTypeName =
-          parentType != null ? ((CtTypeImpl) parentType).getQualifiedName() : "";
+          parentType != null ? convertTypeName(((CtTypeImpl) parentType).getQualifiedName()) : "";
       String parentMemberName =
           parentMember != null ? ((CtTypeMember) parentMember).getSimpleName() : "";
       return Triple.of(ChangeType.UPDATED, parentTypeName, parentMemberName);
     }
     //    String nodeType = element.getClass().getSimpleName();
     //    nodeType = nodeType.substring(2, nodeType.length() - 4);
+  }
+
+  /**
+   * Since Gumtree has $ in its type name
+   *
+   * @return
+   */
+  private static String convertTypeName(String name) {
+    return StringUtils.removeEnd(name, CtTypeImpl.INNERTTYPE_SEPARATOR)
+        .replaceAll("\\" + CtTypeImpl.INNERTTYPE_SEPARATOR, ".")
+        .replaceAll("\\.\\d*$", "");
   }
 }
