47216d6e5ba25ff319acc09ae92f53553959a078
Symbolk <symbolk@163.com>
2021-08-04 15:50:40 +0800

    feat: add xll module with yaml config loader

---
+++ xll/src/main/java/ConfigLoader.java
@@ -0,0 +1,31 @@
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Objects;
+
+public class ConfigLoader {
+  public static void main(String[] args) {
+    ConfigLoader loader = new ConfigLoader();
+    loader.load(Objects.requireNonNull(loader.getClass().getClassLoader().getResource("config.yaml")).getPath());
+  }
+
+  public void load(String path) {
+    InputStream inputStream = null;
+    try {
+      inputStream = new FileInputStream(path);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    if (null != inputStream) {
+      Yaml yaml = new Yaml();
+      Map<String, Object> data = yaml.load(inputStream);
+      System.out.println(data);
+    } else {
+      System.out.println("Error when reading file: " + path);
+    }
+  }
+}
