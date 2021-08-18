21bc0e68f5e6ae00e46a653a9645615bdfeb71c5
Symbolk <symbolk@163.com>
2021-08-18 11:59:41 +0800

    fix: change 'def' to 'protocol' in config and correctly get protocol from label

--- core/src/main/java/edu/pku/code2graph/model/Protocol.java
+++ core/src/main/java/edu/pku/code2graph/model/Protocol.java
@@ -1,5 +1,7 @@
 package edu.pku.code2graph.model;
 
+import java.util.Arrays;
+
 public enum Protocol {
   DEF("def"),
   USE("use"),
@@ -14,5 +16,18 @@ public enum Protocol {
   @Override
   public String toString() {
     return label;
+  }
+
+  /**
+   * Get enum constant from label
+   * Corresponding to Enum.valueOf(name)
+   * @param s
+   * @return
+   */
+  public static Protocol valueOfLabel(String s) {
+    return Arrays.stream(Protocol.values())
+        .filter(p -> p.label.equals(s))
+        .findFirst()
+        .orElse(Protocol.UNKNOWN);
   }
 }
