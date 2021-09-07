78ffdadea6007a30de318276969bf0de26d06039
Symbolk <symbolk@163.com>
2021-09-07 20:13:30 +0800

    chor: refine the framework for evaluation

--- client/src/main/java/edu/pku/code2graph/client/Evaluation.java
+++ client/src/main/java/edu/pku/code2graph/client/Evaluation.java
@@ -5,6 +5,7 @@ import edu.pku.code2graph.model.Language;
 import edu.pku.code2graph.model.Node;
 import edu.pku.code2graph.model.URI;
 import edu.pku.code2graph.xll.Rule;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 import org.jgrapht.Graph;
 
@@ -13,19 +14,32 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Evaluation {
+  // test one repo at a time
+  private static String repoName = "";
+  private static String repoPath = "";
+  private static Code2Graph client = null;
 
   public static void main(String[] args) {
-    //
-    String repoName = "";
-    String repoPath = "";
-
-    Code2Graph client = new Code2Graph(repoName, repoPath, "");
+    // set up
+    repoName = "";
+    repoPath = "";
+    client = new Code2Graph(repoName, repoPath, "");
     client.setSupportedLanguages(
         new HashSet(Arrays.asList(Language.JAVA, Language.HTML, Language.XML)));
-    testRename(client);
+
+    // run evaluation
+    testXLLDetection();
   }
 
-  private static void testRename(Code2Graph client) {
+  /** Run the experiments on real repo, and compare the results with the ground truth */
+  private static void testXLLDetection() {}
+
+  /** Metric: compare output with ground truth and calculate the precision and recall */
+  private static Pair<Double, Double> computePR() {
+    return Pair.of(1.0, 1.0);
+  }
+
+  private static void testRename() {
     Graph<Node, Edge> graph = client.generateGraph();
     System.out.println(graph.vertexSet().size());
     System.out.println(graph.edgeSet().size());
@@ -39,7 +53,7 @@ public class Evaluation {
     // compare, check or evaluate
   }
 
-  private static void testCochange(Code2Graph client) {
+  private static void testCochange() {
     // for a historical multi-lang commit
 
     // given changes in lang A, mask changes in lang B
@@ -48,7 +62,7 @@ public class Evaluation {
 
   }
 
-  private static void testLint(Code2Graph client) {
+  private static void testLint() {
     // for the previous version (parent commit) of an inconsistency fixing commit
 
     // lint
