e988fdf442e3a09014b1029e5f9f3a1bffa9347e
tannpopo <yuailun@pku.edu.cn>
2021-09-22 00:00:11 +0800

    refa: create AbstractExtractor for reuse

--- client/src/main/java/edu/pku/code2graph/client/extractor/SpringExtractor.java
+++ client/src/main/java/edu/pku/code2graph/client/extractor/SpringExtractor.java
@@ -1,6 +1,5 @@
 package edu.pku.code2graph.client.extractor;
 
-import com.csvreader.CsvWriter;
 import edu.pku.code2graph.gen.html.JsoupGenerator;
 import edu.pku.code2graph.gen.html.model.NodeType;
 import edu.pku.code2graph.gen.jdt.AbstractJdtVisitor;
@@ -21,16 +20,13 @@ import org.eclipse.jdt.core.dom.FileASTRequestor;
 import org.jgrapht.Graph;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.*;
 import java.util.List;
 
-public class SpringExtractor {
+public class SpringExtractor extends AbstractExtractor {
   public List<URI> htmlURIS = new ArrayList<>();
   public Map<String, List<URI>> javaURIS = new HashMap<>();
-  public List<Pair<URI, URI>> uriPairs = new ArrayList<>();
 
   private static final String JRE_PATH =
       System.getProperty("java.home") + File.separator + "lib/rt.jar";
@@ -125,20 +121,6 @@ public class SpringExtractor {
     }
   }
 
-  private static List<URI> removeDuplicateOutputField(List<URI> list) {
-    Set<URI> set =
-        new TreeSet<>(
-            (a, b) -> {
-              int compareToResult = 1; // ==0表示重复
-              if (StringUtils.equals(a.toString(), b.toString())) {
-                compareToResult = 0;
-              }
-              return compareToResult;
-            });
-    set.addAll(list);
-    return new ArrayList<>(set);
-  }
-
   private void findPairByHtmlUri(URI uri) {
     for (String key : javaURIS.keySet()) {
       if (uri.getFile().contains(key)) {
@@ -164,51 +146,5 @@ public class SpringExtractor {
     extractJavaUri(repoPath);
     findPairs();
     return uriPairs;
-  }
-
-  public void writeToFile(String filePath) throws IOException {
-    File outFile = new File(filePath);
-    if (!outFile.exists()) {
-      outFile.createNewFile();
-    }
-    CsvWriter writer = new CsvWriter(filePath, ',', Charset.forName("UTF-8"));
-    String[] headers = {"HTML", "JAVA"};
-    writer.writeRecord(headers);
-    for (Pair<URI, URI> pair : uriPairs) {
-      String left = pair.getLeft().toString(), right = pair.getRight().toString();
-      String[] record = {
-        left.substring(5, left.length() - 1), right.substring(5, right.length() - 1)
-      };
-      writer.writeRecord(record);
-    }
-    writer.close();
-  }
-
-  private void findExtInRepo(String path, List<String> exts, List<String> filePaths) {
-    File file = new File(path);
-    LinkedList<File> list = new LinkedList<>();
-    if (file.exists()) {
-      if (null == file.listFiles()) {
-        return;
-      }
-      list.addAll(Arrays.asList(file.listFiles()));
-      while (!list.isEmpty()) {
-        File[] files = list.removeFirst().listFiles();
-        if (null == files) {
-          continue;
-        }
-        for (File f : files) {
-          if (f.isDirectory()) {
-            list.add(f);
-          } else {
-            for (String ext : exts) {
-              if (f.getName().length() > ext.length() && f.getName().endsWith(ext)) {
-                filePaths.add(f.getAbsolutePath());
-              }
-            }
-          }
-        }
-      }
-    }
   }
 }
