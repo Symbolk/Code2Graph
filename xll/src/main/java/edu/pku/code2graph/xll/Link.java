928c42f956c18f45cacaedbc8b40a00daaeef40e
Symbolk <symbolk@163.com>
2021-09-22 16:28:49 +0800

    feat: set supported lang by framework, export detected xll in output

--- xll/src/main/java/edu/pku/code2graph/xll/Link.java
+++ xll/src/main/java/edu/pku/code2graph/xll/Link.java
@@ -2,10 +2,18 @@ package edu.pku.code2graph.xll;
 
 import edu.pku.code2graph.model.URI;
 
+import java.util.Objects;
+
 public final class Link {
   public final URI left;
   public final Rule middle;
   public final URI right;
+
+  public Link(final URI left, final URI right) {
+    this.left = left;
+    this.middle = new Rule();
+    this.right = right;
+  }
 
   public Link(final URI left, final Rule middle, final URI right) {
     this.left = left;
@@ -22,5 +30,13 @@ public final class Link {
     output.append(right.toString());
     output.append(")");
     return output.toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Link link = (Link) o;
+    return Objects.equals(left, link.left) && Objects.equals(right, link.right);
   }
 }
