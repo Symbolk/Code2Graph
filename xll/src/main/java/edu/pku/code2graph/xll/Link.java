453c8be9829844dd0df7a36949a1d16ca1aabf46
Shigma <1700011071@pku.edu.cn>
2022-04-18 04:08:27 +0800

    fix: fix several bugs

--- xll/src/main/java/edu/pku/code2graph/xll/Link.java
+++ xll/src/main/java/edu/pku/code2graph/xll/Link.java
@@ -15,6 +15,11 @@ public final class Link extends LinkBase<URI> {
   }
 
   @Override
+  public Link clone() {
+    return new Link(def, use, rule, input, output);
+  }
+
+  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
