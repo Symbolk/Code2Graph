dc6016e46b368a88d8d019ec9080be278bc0d527
Shigma <shigma10826@gmail.com>
2022-07-18 00:42:26 +0800

    feat: initialize mining package

---
+++ mining/src/main/java/edu/pku/code2graph/mining/History.java
@@ -0,0 +1,9 @@
+package edu.pku.code2graph.mining;
+
+import edu.pku.code2graph.model.URITree;
+
+import java.util.ArrayList;
+
+public class History extends ArrayList<URITree> {
+  public void analyzeDiff(URITree tree1, URITree tree2) {}
+}
