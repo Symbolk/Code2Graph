d6286ad25abe7c0c4d14a185991b55d636beaf0f
Shigma <shigma10826@gmail.com>
2022-07-18 00:44:14 +0800

    feat: diff by commit files

--- mining/src/main/java/edu/pku/code2graph/mining/History.java
+++ mining/src/main/java/edu/pku/code2graph/mining/History.java
@@ -1,22 +1,52 @@
 package edu.pku.code2graph.mining;
 
-import edu.pku.code2graph.model.URI;
 import edu.pku.code2graph.model.URITree;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class History extends ArrayList<URITree> {
-  public void analyzeDiff(URITree head, URITree base, int decay) {
-    Set<URI> additions = new HashSet<>();
-    Set<URI> deletions = new HashSet<>();
-    for (URI uri : head.keySet()) {
-      if (!base.has(uri)) additions.add(uri);
+  public final Map<String, ArrayList<String>> commits = new LinkedHashMap<>();
+  public final Map<String, ArrayList<String>> files = new HashMap<>();
+
+  public void loadAll() {
+    int age = 0;
+    String previous = null;
+    for (String current : commits.keySet()) {
+      diffCommit(current, previous, ++age);
+      previous = current;
     }
-    for (URI uri : base.keySet()) {
-      if (!head.has(uri)) deletions.add(uri);
+  }
+
+  public Diff diffCommit(String head, String base, int age) {
+    ArrayList<String> headFiles = commits.get(head);
+    ArrayList<String> baseFiles = commits.getOrDefault(base, new ArrayList<>());
+    Diff diff = new Diff(age);
+    for (String file : headFiles) {
+      if (baseFiles.contains(file)) continue;
+      for (String uri : files.get(file)) {
+        diff.additions.add(uri);
+      }
     }
-    return;
+    for (String file : baseFiles) {
+      if (headFiles.contains(file)) continue;
+      for (String uri : files.get(file)) {
+        if (diff.additions.contains(uri)) {
+          diff.additions.remove(uri);
+        } else {
+          diff.deletions.add(uri);
+        }
+      }
+    }
+    return diff;
+  }
+
+  public class Diff {
+    public final Set<String> additions = new HashSet<>();
+    public final Set<String> deletions = new HashSet<>();
+    public final int age;
+
+    Diff(int age) {
+      this.age = age;
+    }
   }
 }
