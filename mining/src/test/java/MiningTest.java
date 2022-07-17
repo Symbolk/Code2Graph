a9721878bba0081114dc379456ec26c45f118d0b
Shigma <shigma10826@gmail.com>
2022-07-18 00:43:38 +0800

    test: add basic history loader

--- mining/src/test/java/MiningTest.java
+++ mining/src/test/java/MiningTest.java
@@ -1,8 +1,48 @@
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
 
 public class MiningTest {
+  private static Logger logger = LoggerFactory.getLogger(MiningTest.class);
+
+  public void test(String framework, String repoName) throws IOException {
+    String cacheDir = System.getProperty("user.home") + "/coding/xll/sha-history/" + framework + "/" + repoName;
+    String commitListPath = cacheDir + "/commits.txt";
+
+    FileReader fr = new FileReader(commitListPath);
+    BufferedReader br = new BufferedReader(fr);
+    String line;
+    while ((line = br.readLine()) != null) {
+      System.out.println(line);
+    }
+    br.close();
+    fr.close();
+  }
+
   @Test
-  public void test1() {
-    System.out.println("pass");
+  public void testCloudReader() throws IOException {
+    test("android", "CloudReader");
+  }
+
+  @Test
+  public void testGSYVideoPlayer() throws IOException {
+    test("android", "GSYVideoPlayer");
+  }
+
+  @Test
+  public void testNewPipe() throws IOException {
+    test("android", "NewPipe");
+  }
+
+  @Test
+  public void testVirtualXposed() throws IOException {
+    test("android", "VirtualXposed");
+  }
+
+  @Test
+  public void testXposedInstaller() throws IOException {
+    test("android", "XposedInstaller");
   }
 }
