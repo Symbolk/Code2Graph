bb21830be34ca0c42f1760726036b886c9b7cbc5
Shigma <shigma10826@gmail.com>
2022-07-18 00:44:40 +0800

    feat: evaluate for generated rules

--- mining/src/main/java/edu/pku/code2graph/mining/Baseline.java
+++ mining/src/main/java/edu/pku/code2graph/mining/Baseline.java
@@ -2,21 +2,26 @@ package edu.pku.code2graph.mining;
 
 import edu.pku.code2graph.model.URI;
 import edu.pku.code2graph.model.URITree;
+import edu.pku.code2graph.xll.Link;
+import edu.pku.code2graph.xll.Linker;
+import edu.pku.code2graph.xll.Rule;
+import edu.pku.code2graph.xll.URIPattern;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
 
 public class Baseline {
-  double total = 0;
+  double sum = 0;
   double negative = 0;
   public URITree tree = new URITree();
+  public ArrayList<Rule> rules = new ArrayList<>();
   private HashMap<URI, HashMap<URI, Double>> graph = new HashMap<>();
 
   public void connect(URI a, URI b, double confidence) {
     HashMap<URI, Double> map1 = graph.computeIfAbsent(a, k -> new HashMap<>());
     HashMap<URI, Double> map2 = graph.computeIfAbsent(b, k -> new HashMap<>());
     double value = Confidence.add(map1.getOrDefault(b, 0.), confidence);
-    total = Confidence.add(total, confidence);
+    sum = Confidence.add(sum, confidence);
     if (confidence == 0) negative++;
     map1.put(b, value);
     map2.put(a, value);
@@ -25,9 +30,25 @@ public class Baseline {
   public void normalize() {
     for (URI left : graph.keySet()) {
       for (URI right : graph.keySet()) {
-        double value = graph.get(left).get(right);
-        if (value == 0) graph.get(left).put(right, total / negative);
+        HashMap<URI, Double> map = graph.get(left);
+        double value = map.get(right);
+        if (value == 0) {
+          map.put(right, -sum / negative);
+        }
       }
     }
+  }
+
+  public double evaluate(URIPattern def, URIPattern use) {
+    Linker linker = new Linker(tree, def, use);
+    linker.link();
+    double sum = 0;
+    for (Link link : linker.links) {
+      URI left = link.def;
+      URI right = link.use;
+      double value = graph.get(left).get(right);
+      sum = Confidence.add(sum, value);
+    }
+    return sum;
   }
 }
